9430e07dbaea7f28b415af02ec5faad2b441bde5
HuyDiCode <nbquochuy.forwork@gmail.com>
2024-11-15 19:38:57 +0700

    [HuyNBQ] feat: change to use csv and add more case

--- src/index.js
+++ src/index.js
@@ -1,72 +1,97 @@
-const { Client, IntentsBitField } = require('discord.js');
-require('dotenv').config();
-const ExcelJS = require('exceljs');
+const { Client, IntentsBitField } = require("discord.js");
+const ExcelJS = require("exceljs");
+require("dotenv").config();
 
+// Initialize Discord client
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
     IntentsBitField.Flags.GuildMembers,
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
-  ]
+  ],
 });
 
+// Constants
 const prefix = process.env.DISCORD_PREFIX;
 const token = process.env.DISCORD_TOKEN;
-const excelFilePath = "src/F19.xlsx";
+const excelFilePath = "src/students.csv";
+const validStudentCodeRegex = /^[A-Za-z]{2}\d{6}$/;
+const roleName = "Round 1: Challenger";
+const formattedDate = new Intl.DateTimeFormat("vi-VN", {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+});
 
-client.on('messageCreate', async (message) => {
+client.on("messageCreate", async (message) => {
   if (message.author.bot) return;
 
   if (message.content.startsWith(`${prefix}CHECK_`)) {
-    let studentCode = message.content.slice(7);
+    const studentCode = message.content.slice(7);
 
-    const regex = /^[A-Za-z]{2}\d{6}$/;
-
-    if (regex.test(studentCode)) {
+    if (validStudentCodeRegex.test(studentCode)) {
       try {
         const workbook = new ExcelJS.Workbook();
-        await workbook.xlsx.readFile(excelFilePath);
+        await workbook.csv.readFile(excelFilePath);
+        const worksheet = workbook.getWorksheet(1);
+        const member = message.guild.members.cache.get(message.author.id);
 
-        const worksheet = workbook.getWorksheet(1); // Assuming data is in the first sheet
-        
-        // Find the student in the Excel file
-        let foundStudent = 0;
-        worksheet.eachRow(function(row, rowNumber) {
-          if(row.getCell('E').value.toUpperCase() === studentCode.toUpperCase() && row.getCell('I').value === 0)
-            foundStudent = rowNumber;
+        if (!member.displayName.toUpperCase().includes(studentCode)) {
+          return message.reply("Vui lòng đặt tên đúng quy tắc.");
+        }
+
+        let foundRow = null;
+        worksheet.eachRow((row) => {
+          if (
+            row.getCell("A").value.toUpperCase() === studentCode.toUpperCase()
+          ) {
+            foundRow = row;
+          }
         });
 
-        if (foundStudent !== 0) {
-          // Mark the student as joined in the Excel file
-          rowStudent = worksheet.getRow(foundStudent)
-          rowStudent.getCell(9).value = "1"
+        if (foundRow) {
+          if (foundRow.getCell("B").value === 0) {
+            foundRow.getCell("B").value = "1";
+            foundRow.getCell("C").value = new Date().toString();
+            await workbook.csv.writeFile(excelFilePath);
 
-          // Save the changes to the Excel file
-          await workbook.xlsx.writeFile(excelFilePath);
+            const role = message.guild.roles.cache.find(
+              (r) => r.name === roleName
+            );
 
-          // Add role to the Discord member
-          const member = message.guild.members.cache.get(message.author.id);
-          const role = message.guild.roles.cache.find(role => role.name === 'Challenger 1');
-
-          if (role && member) {
-            await member.roles.add(role);
-            message.reply('Đã thêm roll cho bạn.');
+            if (role && member) {
+              await member.roles.add(role);
+              return message.reply("Đã thêm role cho bạn.");
+            } else {
+              return message.reply("Không tìm thấy thành viên.");
+            }
+          } else {
+            return message.reply(
+              `Bạn đã xác nhận thành công vào lúc ${formattedDate.format(
+                new Date(foundRow.getCell("C").value)
+              )}`
+            );
           }
         } else {
-          message.reply('Mã số sinh viên này đã được xác nhận hoặc không có.');
+          return message.reply("Mã số sinh viên này không tồn tại.");
         }
       } catch (error) {
-        console.error('Error accessing Excel file:', error);
-        message.reply('Có một số lỗi đã xảy ra bạn thử lại trong ít phút nhé!');
+        console.error("Error accessing Excel file:", error);
+        return message.reply(
+          "Có một số lỗi đã xảy ra bạn thử lại trong ít phút hoặc tạo ticket để được hỗ trợ nhé!"
+        );
       }
     } else {
-      message.reply('Mã số sinh viên không hợp lệ.');
+      return message.reply("Mã số sinh viên không hợp lệ.");
     }
   }
 });
 
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Bot đã sẵn sàng với tên: ${client.user.tag}`);
 });
 
