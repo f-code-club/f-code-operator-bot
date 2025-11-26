a481d82ecf06ff66061c742ec942f814ac33b177
akagiyuu <khanghuynh0245@gmail.com>
2025-11-26 13:16:46 +0700

    feat(database): run migration on startup

--- src/state.rs
+++ src/state.rs
@@ -10,6 +10,7 @@ impl State {
     #[tracing::instrument]
     pub async fn new(database_url: &str) -> Result<Self> {
         let pool = SqlitePool::connect(database_url).await?;
+        sqlx::migrate!().run(&pool).await.unwrap();
 
         Ok(State { pool })
     }
