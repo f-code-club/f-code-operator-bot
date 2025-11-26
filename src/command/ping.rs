fc4c8a0c6a5e291bd199c7fba319ded994ffea5a
akagiyuu <khanghuynh0245@gmail.com>
2025-11-26 21:09:09 +0700

    doc: add commands doc

--- src/command/ping.rs
+++ src/command/ping.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 
 use crate::Context;
 
+/// Test if bot is responsive
 #[tracing::instrument]
 #[poise::command(slash_command, prefix_command, ephemeral)]
 pub async fn ping(ctx: Context<'_>) -> Result<()> {
