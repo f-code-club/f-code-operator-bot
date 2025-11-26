8b1863089150694698db4383a0b30d7550cea28d
akagiyuu <khanghuynh0245@gmail.com>
2025-11-26 12:01:35 +0700

    feat: init tracing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+pub mod state;
+pub mod config;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .pretty()
+        .with_timer(tracing_subscriber::fmt::time::ChronoLocal::rfc_3339())
+        .init();
 }
